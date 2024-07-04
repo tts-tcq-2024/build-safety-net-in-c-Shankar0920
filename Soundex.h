19c27c077e792d4278eccadf2d4477bff4b6dd65
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-04 14:44:15 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,37 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
 char getSoundexCode(char c) {
     c = toupper(c);
-    if (c == 'B' || c == 'F' || c == 'P' || c == 'V') {
-        return '1';
-    } else if (c == 'C' || c == 'G' || c == 'J' || c == 'K' || c == 'Q' || c == 'S' || c == 'X' || c == 'Z') {
-        return '2';
-    } else {
-        return '3';
+    switch (c) {
+        case 'B': case 'F': case 'P': case 'V': return '1';
+        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
+        case 'D': case 'T': return '3';
+        case 'L': return '4';
+        case 'M': case 'N': return '5';
+        case 'R': return '6';
+        default: return '0'; // For A, E, I, O, U, H, W, Y
     }
 }
 void generateSoundex(const char *name, char *soundex) {
     int len = strlen(name);
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
-    char prevCode = getSoundexCode(name[0]);
 
     for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        if (code != '3' && code != prevCode) {
+        if (code != '0' && code != soundex[sIndex - 1]) {
             soundex[sIndex++] = code;
         }
-        prevCode = code;
     }
-
     while (sIndex < 4) {
         soundex[sIndex++] = '0';
     }
 
     soundex[4] = '\0';
 }
-
 int main() {
-    const char *name = "Soundex";
+    const char *name = "Washington";
     char soundex[5];
     generateSoundex(name, soundex);
     printf("Soundex code for '%s' is '%s'\n", name, soundex);
