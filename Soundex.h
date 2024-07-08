e6f1bba29a79041597ba97a58765ace86125411b
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-08 11:35:56 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,6 +1,10 @@
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include "Soundex.h"
 char getSoundexCode(char c) {
     c = toupper(c);
     // Define a mapping from characters to their corresponding Soundex codes
@@ -18,27 +22,21 @@ char getSoundexCode(char c) {
         return '0'; // Default case for characters not handled
     }
 }
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    // Ensure the first character of soundex is uppercase
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1; // Index for soundex array
-    char lastCode = getSoundexCode(name[0]); // Soundex code of the first character
-    
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char currentChar = toupper(name[i]);
-        char currentCode = getSoundexCode(currentChar);
-        if (currentCode == '0') {
-            continue; // Skip invalid Soundex codes
-        }
-        if (currentCode != lastCode) {
-            soundex[sIndex++] = currentCode; // Add the code to soundex
-            lastCode = currentCode; // Update lastCode
-        }
+
+void generateSoundex(const char* name, char* soundex) {
+    if (strlen(name) == 0) {
+        soundex[0] = '\0';
+        return;
     }
-    // Fill any remaining positions in soundex with '0'
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }  
-    soundex[4] = '\0'; // Null-terminate the soundex string
+
+    soundex[0] = toupper(name[0]);
+    char prevCode = getSoundexCode(name[0]);
+
+    prevCode = getStringCode(name, soundex, prevCode);
+
+    while (strlen(soundex) < 4) {
+        soundex[strlen(soundex)] = '0';
+    }
+    soundex[4] = '\0';
 }
+#endif // SOUNDEX_H
