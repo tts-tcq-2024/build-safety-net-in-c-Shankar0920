d2c70b8b34a9d8b3398d5667c7dd0b53c8b51418
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-08 14:49:01 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -26,13 +26,10 @@ void initializeSoundex(char* soundex, char firstChar)
 {
     soundex[0] = toupper(firstChar);
 }
-void padSoundex(char* soundex,const char* name)
+void padSoundex(char* soundex)
 {
     size_t i = strlen(soundex);
-    if (name[0] == '\0')
-    {
-        result = "";
-    }
+    
     while (i < 4)
     {
         soundex[i] = '0';
