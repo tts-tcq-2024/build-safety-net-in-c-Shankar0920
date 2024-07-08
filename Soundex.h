d9a9393f9416bb237a11547df3f9eb064458ad3e
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-08 15:18:47 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,18 +1,14 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include "Soundex.h"
 
 char getSoundexCode(char c)
 {
     static const char soundexCodes[26] = {'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'};
     return soundexCodes[toupper(c) - 'A'];
 }
-
-char algorithmCheck(char prevcode, char* soundex, size_t i, const char* name)
+ 
+char eliminateZeroAndRepeatedValue(char prevcode, char* soundex, size_t i, const char* name)
 {
     char code = getSoundexCode(name[i]);
     if (code != '0' && code != prevcode)
@@ -22,35 +18,33 @@ char algorithmCheck(char prevcode, char* soundex, size_t i, const char* name)
     }
     return prevcode;
 }
-char soundexCode(const char* name, char* soundex, char prevCode,size_t i)
-{
- while (name[i] != '\0' && i < 4)
-    {
-        prevCode = algorithmCheck(prevCode, soundex, i, name);
-        i++;
-    }
-    return prevCode;
-}
+ 
 void initializeSoundex(char* soundex, char firstChar)
 {
     soundex[0] = toupper(firstChar);
 }
-void padSoundex(char soundex)
+ 
+void padSoundex(char* soundex)
 {
-    size_t i = strlen(soundex);
-    while (i < 4)
+    size_t len = strlen(soundex);
+    while (len < 4)
     {
-        soundex[i] = '0';
-        i++;
+        soundex[len] = '0';
+        len++;
     }
     soundex[4] = '\0'; // Ensure soundex is null-terminated
 }
+ 
 void generateSoundex(const char* name, char* soundex)
 {
     initializeSoundex(soundex, name[0]);
     char prevCode = getSoundexCode(name[0]);
+ 
     size_t i = 1;
-    char soundex_1=soundexCode(name,soundex,prevCode,i);
-    padSoundex(soundex_1);
+    while (name[i] != '\0' && i < 4)
+    {
+        prevCode = eliminateZeroAndRepeatedValue(prevCode, soundex, i, name);
+        i++;
+    }
+    padSoundex(soundex);
 }
-#endif
