0ea283d2031df6b003928c181aa6334277f8af88
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-23 11:18:19 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,17 +1,44 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
-
-TEST(SoundexTest, HandlesEmptyString)
-{
-    EXPECT_EQ(generateSoundex(""), "");
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+//AAA
+  char soundex[5];
+generateSoundex("$rrr", soundex);
+ASSERT_STREQ(soundex,"$666");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+//AAA
+char soundex[5];
+generateSoundex("pvt", soundex);
+ASSERT_STREQ(soundex,"P130");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+//AAA
+  char soundex[5];
+generateSoundex("*#12hi", soundex);
+ASSERT_STREQ(soundex,"*000");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_vowels) {
+//AAA
+char soundex[5];
+generateSoundex("aeiou", soundex);
+ASSERT_STREQ(soundex,"A000");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) 
-{
-    EXPECT_EQ(generateSoundex("A"), "A000");
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_space) {
+//AAA
+char soundex[5];
+generateSoundex(" ", soundex);
+ASSERT_STREQ(soundex," 000");
 }
-
-TEST(SoundexTest, HandlesString)
-{
-    EXPECT_EQ(generateSoundex("HERMAN"), "H650");
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+//AAA
+char soundex[5];
+generateSoundex("bcdlmnr", soundex);
+ASSERT_STREQ(soundex,"B234");
 }
